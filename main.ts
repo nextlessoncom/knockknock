2a77a346237eca1f209f4a7034e3ebaa5b67edcc
nextlessoncom <102201655+nextlessoncom@users.noreply.github.com>
2022-03-22 23:24:07 -0400

    initial characters

--- main.ts
+++ main.ts
@@ -1,1 +1,4 @@
-
+let Mario = sprites.create(assets.image`Mario`, SpriteKind.Player)
+let Luigi = sprites.create(assets.image`Luigi`, SpriteKind.Player)
+Mario.setPosition(12, 93)
+Luigi.setPosition(46, 93)
